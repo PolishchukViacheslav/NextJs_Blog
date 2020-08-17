da8abd1b6545c0d5644862329a5f2df716d4830f
Slava Polishchuk <dizerllc@gmail.com>
2020-08-17 11:39:32 +0300

    context del

--- redux/store.ts
+++ redux/store.ts
@@ -2,10 +2,10 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { reducer } from './reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { MakeStore, createWrapper, Context } from 'next-redux-wrapper';
+import { MakeStore, createWrapper } from 'next-redux-wrapper';
 import { State } from '../interfaces';
 
-export const makeStore: MakeStore<State> = (context: Context) =>
+export const makeStore: MakeStore<State> = () =>
   createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
 export const wrapper = createWrapper<State>(makeStore);
